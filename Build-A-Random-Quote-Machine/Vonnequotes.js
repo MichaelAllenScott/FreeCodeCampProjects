15d9ab0d1ad2cc240477c245b5848ba41f33e9a3
MichaelAllenScott <michaelallenscott55@gmail.com>
2017-04-28 12:45:43 -0400

    1.0 Random Quote Machine Complete

--- Build-A-Random-Quote-Machine/Vonnequotes.js
+++ Build-A-Random-Quote-Machine/Vonnequotes.js
@@ -22,3 +22,12 @@ function newQuoteFound(jsonQuote){
 function errorFindingQuote(msg){
     console.log(msg);
 }
+
+function tweetQuote(){
+    openURL('https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' 
+    + encodeURIComponent('"' + document.getElementById("quote").innerHTML + '"'));
+}
+
+function openURL(url){
+  window.open(url, 'Share', 'width=550, height=400, toolbar=0, scrollbars=1 ,location=0 ,statusbar=0,menubar=0, resizable=0');
+}

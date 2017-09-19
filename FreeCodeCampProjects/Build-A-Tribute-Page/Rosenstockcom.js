58c79c66ec1227e65ad67e71aa3094d7b1e97219
Michael Allen Scott <michaelallenscott55@gmail.com>
2017-09-18 22:01:38 -0400

    BATP - Torn paper effects

--- FreeCodeCampProjects/Build-A-Tribute-Page/Rosenstockcom.js
+++ FreeCodeCampProjects/Build-A-Tribute-Page/Rosenstockcom.js
@@ -1,3 +1,8 @@
+function SetupDynamicElements() {
+    SetListStyleImages();
+    SetTornPaperImages();
+}
+
 function SetListStyleImages() {
     const LightningBoltPath = "url('Assets/CustomBullets/LightningBolt.png')";
     const StarPath = "url('Assets/CustomBullets/Star.png')";
@@ -14,6 +19,29 @@ function SetListStyleImages() {
         if(liElements[i].textContent == "5 Funerals" && liElements[i].style.listStyleImage == 'url("Assets/CustomBullets/Skull.png")') {
             liElements[i].style.listStyleImage = LightningBoltPath;
         }
+    }
+}
+
+function SetTornPaperImages() {
+    const topImagePaths = new Array(
+        "Assets/TornPaperEffects/torn-paper-top-1.png", 
+        "Assets/TornPaperEffects/torn-paper-top-2.png"
+    );
+
+    const bottomImagePaths = new Array(
+        "Assets/TornPaperEffects/torn-paper-bottom-1.png", 
+        "Assets/TornPaperEffects/torn-paper-bottom-2.png"
+    );
+
+    var topTornPaperElements = document.getElementsByClassName("TopTornPaper");
+    var bottomTornPaperElements = document.getElementsByClassName("BottomTornPaper");
+
+    for(var i = 0; i < topTornPaperElements.length; i++) {
+        topTornPaperElements[i].src = topImagePaths[Math.floor(Math.random() * topImagePaths.length)];
+    }
+
+    for(var i = 0; i < bottomTornPaperElements.length; i++) {
+        bottomTornPaperElements[i].src = bottomImagePaths[Math.floor(Math.random() * bottomImagePaths.length)];
     }
 }
 
